10eba49487c0b052bee23a58423650f34caea047
Jonathan Wong <jonathanfwong@gmail.com>
2023-08-01 18:50:19 +1000

    commit

---
+++ firebase.ts
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp, getApp, getApps } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
+import { getAuth } from 'firebase/auth'
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB1AiSvvyyfJhHMQv1ydPiK8LZb7um_dsg",
+  authDomain: "netflix-clone-yt-bb46c.firebaseapp.com",
+  projectId: "netflix-clone-yt-bb46c",
+  storageBucket: "netflix-clone-yt-bb46c.appspot.com",
+  messagingSenderId: "753357081734",
+  appId: "1:753357081734:web:55838ef4effecf12685507"
+}
+// Initialize Firebase
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp()
+const db = getFirestore()
+const auth = getAuth()
+
+export default app
+export { auth, db }
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,10 @@ import Banner from '@/components/Banner'
 import requests from '@/utils/requests'
 import { Movie } from '@/typings'
 import Row from '@/components/Row'
+import useAuth from '@/hooks/useAuth'
+import { useRecoilValue } from 'recoil'
+import { modalState } from '@/atoms/modalAtom'
+import Modal from '@/components/Modal'
 
 interface Props {
   netflixOriginals: Movie[]
@@ -26,6 +30,11 @@ export default function Home({
   topRated,
   trendingNow,
 }: Props) {
+  const { loading } = useAuth();
+  const showModal = useRecoilValue(modalState)
+
+  if(loading) return <div className='w-screen h-screen bg-[#141414]'></div>
+
   return (
     <div className='relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]'>
       <Head>
@@ -46,7 +55,7 @@ export default function Home({
           <Row title="Documentaries" movies={documentaries} />
         </section>
       </main>
-      {/* Modal */}
+      {showModal && <Modal />}
     </div>
   )
 }
